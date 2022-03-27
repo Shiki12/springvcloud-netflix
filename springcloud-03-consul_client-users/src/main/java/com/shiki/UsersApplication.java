c3e83d202854ce8717a015db872f866a33d7f419
Shiki <941728409@qq.com>
2022-03-27 16:23:21 +0800

    second commit

--- springcloud-03-consul_client-users/src/main/java/com/shiki/UsersApplication.java
+++ springcloud-03-consul_client-users/src/main/java/com/shiki/UsersApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class UsersApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class,args);
+        System.out.println("git first");
     }
 
 }
